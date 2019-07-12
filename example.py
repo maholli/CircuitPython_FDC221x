0e17f3d1a0888b17cf5c8a742b31e1ebc119559f
Max Holliday <maholli@stanford.edu>
2019-07-12 11:30:47 -0700

    quick example

---
+++ example.py
@@ -0,0 +1,11 @@
+import fdc2212,board,busio
+import time
+
+i2c = busio.I2C(board.SCL,board.SDA)
+cap = fdc2212.FDC2212(i2c, debug=False)
+
+cap.differential = False
+while True:
+    print(time.monotonic(),'{:.9E}'.format(cap.read()))
+    time.sleep(1)
+
